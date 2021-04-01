a919b4731d9d6c2d31604841b0ff14a9f4f98003
VinhChu <vinhchuthe@admicro.vn>
2021-04-01 17:26:50 +0700

    update code

--- website/assets/js/index.js
+++ website/assets/js/index.js
@@ -1,18 +1,62 @@
+window.onbeforeunload = function () {
+    window.scrollTo(0, 0);
+}
+
 $.noConflict();
 jQuery(document).ready(function ($) {
     // Code that uses jQuery's $ can follow here.
 
-    const tilt = $('.block').tilt({
-        perspective: 3000,
-        maxTilt: 10,
-        speed: 200,
+    // nicescroll
+    $("#main-container").niceScroll({
+        scrollspeed: 100,
+        mousescrollstep: 25,
     });
+    $("#popup-detail").niceScroll();
 
-    $(".block--object").click(function () {
-        var index = $(this).data('id');
-        console.log(index);
+    // AOS lib
+    AOS.init();
+
+    // tilt js
+    const tilt = $('.block').tilt({
+        perspective: 2000,
+        maxTilt: 20,
+        speed: 500,
     });
 
     // popup
-    $("#popup-detail").niceScroll();
+
+    // ---- popup-open -------
+    $(".block--object").click(function () {
+        $("#main-container").getNiceScroll().remove();
+        var index = $(this).data('id');
+        popupOpen(index);
+    });
+
+    popupOpen = (index) => {
+        $(".adm_emag").addClass('popup-open');
+        $(".adm--popup").addClass('popup-wrapper-active');
+        $("#" + index).addClass('popup-active');
+    }
+
+    $('.popup-nav').click(function () {
+        var id = $(this).data('nav');
+        $("#popup-detail").animate({
+            scrollTop: 0
+        }, 1000);
+
+        setTimeout(function () {
+            $(".adm-popup--block").removeClass('popup-active');
+            $("#" + id).addClass('popup-active');
+            var change = new TimelineMax();
+        }, 1000);
+    });
+
+
+    //  ------ popup-close -------
+    $("#ss-btn-back").click(function () {
+        $(".adm_emag").removeClass('popup-open');
+        $(".adm--popup").removeClass('popup-wrapper-active');
+        $(".adm-popup--block").removeClass('popup-active');
+        $("#main-container").niceScroll();
+    });
 });
