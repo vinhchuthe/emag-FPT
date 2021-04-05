2940e70a840f97431a0b4a7e98f8efcd8e3beb0f
VinhChu <vinhchuthe@admicro.vn>
2021-04-05 17:07:20 +0700

    update mb ver

---
+++ mobile/assets/js/index.js
@@ -0,0 +1,6 @@
+$(".btn-readmore a").on('click', function () {
+    $('.adm_emag .adm-blog-list').addClass('active');
+});
+$(".btn-back-home a").on('click', function () {
+    $('.adm_emag .adm-blog-list').removeClass('active');
+});
